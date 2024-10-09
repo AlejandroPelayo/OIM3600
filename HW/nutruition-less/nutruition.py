071641636b258e2c4fc6a5894d7da736310d94da
AlejandroPelayo <apelayo1@babson.edu>
2024-10-08 20:48:54 -0400

    Create nutruition.py
    Got help from ChatGPT because I forgot fruit_calories[Pick_fruit] in print("Calories:", fruit_calories[Pick_fruit])

---
+++ HW/nutruition-less/nutruition.py
@@ -0,0 +1,37 @@
+def nutrition():
+    """
+    Copy of Dictionary given with fruit and calories
+    """
+    fruit_calories = {
+        "apple": 130,
+        "avocado": 50,
+        "banana": 110,
+        "cantaloupe": 50,
+        "grapefruit": 60,
+        "grapes": 90,
+        "honeydew": 50,
+        "kiwifruit": 90,
+        "lemon": 15,
+        "lime": 20,
+        "nectarine": 60,
+        "orange": 80,
+        "peach": 60,
+        "pear": 100,
+        "pineapple": 50,
+        "plums": 70,
+        "strawberries": 50,
+        "sweet cherries": 100,
+        "tangerine": 50,
+        "watermelon": 80
+    }
+    Pick_fruit = input("Enter Fruit: ").lower()
+
+    if Pick_fruit in fruit_calories:
+        print("Calories:", fruit_calories[Pick_fruit])
+
+    else:
+        print("Fruit not found")
+
+
+nutrition()
+
